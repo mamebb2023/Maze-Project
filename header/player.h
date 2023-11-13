8d76d94df813d17205fd921a34c7cfb528525a87
Mohammednur <117838736+mamebb2023@users.noreply.github.com>
2023-11-13 13:41:13 +0300

    Update player.h

--- header/player.h
+++ header/player.h
@@ -4,8 +4,7 @@
 void draw_player(SDL_Instance *sdl, player *player);
 void rotate_player(player *player, SDL_Point *mouse);
 SDL_Point move_player(player *player);
-void player_collision_detection(player *player, map_t *map);
-int quadrant_of_angle(int angle, int *resulting_angle);
 void slide_on_wall(player *player);
+int quadrant_of_angle(int angle, int *resulting_angle);
 
 #endif

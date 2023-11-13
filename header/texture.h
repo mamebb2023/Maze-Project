257c7074aaaa648c5ac204628838fe8fdc4dc4f0
Mohammednur <117838736+mamebb2023@users.noreply.github.com>
2023-11-13 13:46:35 +0300

    Update texture.h

--- header/texture.h
+++ header/texture.h
@@ -1,12 +1,13 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
-void textured_sky(SDL_Instance *sdl, map_t *map);
+// texture.c
 void shade_walls(SDL_Color *color_ref, int orientation);
+void texture_sky(SDL_Instance *sdl, map_t *map);
 void texture_walls(SDL_Instance *sdl, double line_height,
 		int column, int y_pos, uint8_t alpha);
-void weapon_gun(SDL_Instance *sdl, map_t *map);
 int render_thread(void *td);
+void weapon_gun(SDL_Instance *sdl, map_t *map);
 
 #endif /* TEXTURE_H */
 

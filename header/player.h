c6e948173b2a863aec3e596287390b4aab76ef73
Mohammednur <117838736+mamebb2023@users.noreply.github.com>
2023-11-13 13:45:28 +0300

    Update player.h

--- header/player.h
+++ header/player.h
@@ -1,10 +1,11 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+// player.c
 void draw_player(SDL_Instance *sdl, player *player);
 void rotate_player(player *player, SDL_Point *mouse);
 SDL_Point move_player(player *player);
 void slide_on_wall(player *player);
 int quadrant_of_angle(int angle, int *resulting_angle);
 
-#endif
+#endif /* PLAYER_H */
